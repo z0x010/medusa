aabc452700f380b6d52ddf612b3dfe426f0f7f3e
gaohaoning <gaohaoning@breadtrip.com>
2015-12-19 17:32:11 +0800

    code update

--- medusaspider/medusaspider/spiders/movie_spider.py
+++ medusaspider/medusaspider/spiders/movie_spider.py
@@ -7,6 +7,7 @@ $ scrapy crawl movie_spider -o db.json
 
 import scrapy
 import re
+import inspect
 from medusaspider.items import MovieItem
 
 
@@ -17,28 +18,26 @@ class MovieSpider(scrapy.spiders.Spider):
     ]
 
     def parse(self, response):
+        """
+        a callback method:
+            extract some items,
+            looks for a link to follow to the next page,
+            and then yields a Request with the same callback for it;
+        """
         # print response
+        # print response.headers
         # print response.body
+        # print response.meta
+        # print response.selector
         # print dir(response)
-        # print 'response.body', response.body
-        # print 'response.body_as_unicode', response.body_as_unicode
-        # print 'response.copy', response.copy
-        # print 'response.css', response.css
-        # print 'response.encoding', response.encoding
-        # print 'response.flags', response.flags
-        # print 'response.headers', response.headers
-        # print 'response.meta', response.meta
-        # print 'response.replace', response.replace
-        # print 'response.request', response.request
-        # print 'response.selector', response.selector
-        # print 'response.status', response.status
-        # print 'response.url', response.url
-        # print 'response.urljoin', response.urljoin
-        # print 'response.xpath', response.xpath
+        # print inspect.getmembers(response)
 
-        # response.selector.xpath():
-        # returns a list of selectors, each of which represents the nodes selected by the xpath expression
+        # [1][extract data from the page]
         for selector in response.selector.xpath('//ol[@class="grid_view"]/li/div[@class="item"]'):
+            """
+            response.selector.xpath():
+            returns a list of selectors, each of which represents the node selected by the xpath expression.
+            """
             print '↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ (in parse)'
             rank = selector.xpath('div[@class="pic"]/em/text()').extract()[0]
             pic = selector.xpath('div[@class="pic"]/a/img/@src').extract()[0]
@@ -68,12 +67,23 @@ class MovieSpider(scrapy.spiders.Spider):
             print '↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ (in parse)'
             yield item
 
-        # next page
+        # [1][extract links from the page, follow them, and then extract data from the pages]
         print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
         next_page = response.xpath('//span[@class="next"]/a/@href').extract()
         # [u'?start=200&filter=']
         if next_page:
             next_url = response.urljoin(next_page[0])
             # http://movie.douban.com/top250?start=25&filter=
+            """
+            Scrapy’s mechanism of following links:
+            when you yield a Request in a callback method,
+            Scrapy will schedule that request to be sent,
+            and register a callback method to be executed when that request finishes.
+            """
+            """
+            This creates a sort of loop,
+            following all the links to the next page until it doesn’t find one
+            – handy for crawling blogs, forums and other sites with pagination.
+            """
             yield scrapy.Request(next_url, self.parse)
         print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'

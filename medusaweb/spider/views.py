3757a017b22501ec1abf27b941b28c809240cc1a
gaohaoning <gaohaoning@breadtrip.com>
2015-12-26 17:54:54 +0800

    django web page ok, static files access ok.

--- medusaweb/spider/views.py
+++ medusaweb/spider/views.py
@@ -1,1 +1,36 @@
-# Create your views here.
+#!/usr/bin/env python
+# coding: utf-8
+
+import datetime
+import random
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template import Template,Context
+from django.template.loader import get_template
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.generic import View
+from django.core.urlresolvers import reverse
+from django.core.files.uploadedfile import UploadedFile, InMemoryUploadedFile, TemporaryUploadedFile
+from django.core.files import File
+from django.core.files.images import ImageFile
+
+
+from coffin.shortcuts import render as coffin_render
+class Jinja2View(View):
+    """
+    使用 Jinja2 模板
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        print '------------------------------------------------------------'
+        context.update(coffin_version='0.4.0')
+        context.update(none_value=None)
+        print context
+        # {'coffin_version': '0.4.0', 'none_value': None}
+        """
+        跟 Django 模板不同，Jinja2 模板会将 None 显示为字符串 "None"
+        """
+        print '------------------------------------------------------------'
+        return coffin_render(request, 'jinja2.html', context)
--- medusaweb/urls.py
+++ medusaweb/urls.py
@@ -1,10 +1,13 @@
+# coding: utf-8
+
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'medusaweb.views.home', name='home'),
     # url(r'^medusaweb/', include('medusaweb.foo.urls')),
@@ -14,4 +17,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+
+from spider import views
+urlpatterns += patterns(
+    '',
+    # Jinja2 模板
+    url(r'^jinja2/$', views.Jinja2View.as_view(), name='jinja2'),
 )

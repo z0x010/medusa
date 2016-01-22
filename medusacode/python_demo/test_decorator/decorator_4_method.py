1013e82c072f3b75828cff5d77a3711c04f3a82b
gaohaoning <gaohaoning@breadtrip.com>
2016-01-22 20:04:07 +0800

    add dir test_decorator

---
+++ medusacode/python_demo/test_decorator/decorator_4_method.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+def method_friendly_decorator(method_to_decorate):
+    def wrapper(self, lie):
+        # very friendly, decrease age even more :-)
+        lie = lie - 3
+        return method_to_decorate(self, lie)
+    return wrapper
+
+
+class Lucy(object):
+
+    def __init__(self):
+        self.age = 32
+
+    @method_friendly_decorator
+    def sayYourAge(self, lie):
+        print "I am %s, what did you think?" % (self.age + lie)
+
+l = Lucy()
+l.sayYourAge(-3)
+# outputs: I am 26, what did you think?

51d7e282fdfa9ef4412589787e2b4ccedbb20f6f
gaohaoning <gaohaoning@breadtrip.com>
2016-01-22 16:59:08 +0800

    add decorator_principle

---
+++ medusacode/python_demo/test_decorator_demo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+def bread(func):
+    def wrapper():
+        print "</''''''\>"
+        func()
+        print "<\______/>"
+    return wrapper
+
+def ingredients(func):
+    def wrapper():
+        print "#tomatoes#"
+        func()
+        print "~salad~"
+    return wrapper
+
+def sandwich(food="--ham--"):
+    print food
+
+sandwich_1 = bread(ingredients(sandwich))
+sandwich_1()
+# outputs:
+# </''''''\>
+# #tomatoes#
+# --ham--
+# ~salad~
+# <\______/>
+
+
+@bread
+@ingredients
+def sandwich_2(food="--ham--"):
+    print food
+
+sandwich_2()
+# outputs:
+# </''''''\>
+# #tomatoes#
+# --ham--
+# ~salad~
+# <\______/>
+
---
+++ medusacode/python_demo/test_decorator_principle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+# A decorator is a function that expects ANOTHER function as parameter
+def my_shiny_new_decorator(a_function_to_decorate):
+
+    # Inside, the decorator defines a function on the fly: the wrapper.
+    # This function is going to be wrapped around the original function
+    # so it can execute code before and after it.
+    def the_wrapper_around_the_original_function():
+
+        # Put here the code you want to be executed BEFORE the original
+        # function is called
+        print "Before the function runs"
+
+        # Call the function here (using parentheses)
+        a_function_to_decorate()
+
+        # Put here the code you want to be executed AFTER the original
+        # function is called
+        print "After the function runs"
+
+    # At this point, "a_function_to_decorate" HAS NEVER BEEN EXECUTED.
+    # We return the wrapper function we have just created.
+    # The wrapper contains the function and the code to execute before
+    # and after. It’s ready to use!
+    return the_wrapper_around_the_original_function
+
+# Now imagine you create a function you don't want to ever touch again.
+def a_stand_alone_function():
+    print "I am a stand alone function, don't you dare modify me"
+
+# Well, you can decorate it to extend its behavior.
+# Just pass it to the decorator, it will wrap it dynamically in
+# any code you want and return you a new function ready to be used:
+
+a_stand_alone_function_decorated = my_shiny_new_decorator(a_stand_alone_function)
+a_stand_alone_function_decorated()
+# outputs:
+# Before the function runs
+# I am a stand alone function, don't you dare modify me
+# After the function runs
+
+@my_shiny_new_decorator
+def another_stand_alone_function():
+    print "Leave me alone"
+
+another_stand_alone_function()
+# outputs:
+# Before the function runs
+# Leave me alone
+# After the function runs
+

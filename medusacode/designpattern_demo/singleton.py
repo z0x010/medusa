137b24201da399fc04a4e263c696bbebf557106c
gaohaoning <gaohaoning@breadtrip.com>
2016-02-02 21:42:26 +0800

    add singleton demo 2

--- medusacode/designpattern_demo/singleton.py
+++ medusacode/designpattern_demo/singleton.py
@@ -2,6 +2,9 @@
 # coding:utf-8
 
 
+"""
+单例模式实现方法
+"""
 print '===================================================================================================='
 """
 方法1
@@ -15,6 +18,7 @@ print '=========================================================================
 在 Python 中，
 只有类属性可以紧跟在类定义之后定义，数据属性定义在__init__方法中。
 """
+
 class Singleton(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
@@ -26,11 +30,54 @@ class MyClass(Singleton):
 
 a = MyClass()
 b = MyClass()
+
 print a, id(a)
 print b, id(b)
 # <__main__.MyClass object at 0x102113a50> 4329650768
 # <__main__.MyClass object at 0x102113a50> 4329650768
 print '===================================================================================================='
+"""
+方法2
+共享属性,所谓单例就是所有引用(实例、对象)拥有相同的状态(属性)和行为(方法)
+同一个类的所有实例天然拥有相同的行为(方法),
+只需要保证同一个类的所有实例具有相同的状态(属性)即可。
+所有实例共享属性的最简单最直接的方法就是__dict__属性指向(引用)同一个字典(dict)。
+"""
+"""
+object.__dict__
+    A dictionary or other mapping object used to store an object’s (writable) attributes.
+注意区分:
+    class.__dict__
+    instance.__dict__
+"""
+
+class Borg(object):
+    _shared_state = {}
+
+    def __new__(cls, *args, **kwargs):
+        obj = super(Borg, cls).__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls._shared_state  # instance.__dict__ = class._shared_state
+        return obj
+
+class MyClass2(Borg):
+    x = 1
+    pass
+
+a = MyClass2()
+b = MyClass2()
+
+a.x = 9
+
+print a, id(a)
+print b, id(b)
+# <__main__.MyClass2 object at 0x10defec50> 4528794704
+# <__main__.MyClass2 object at 0x10defecd0> 4528794832
+print a.x, id(a.x), a.__dict__, id(a.__dict__), id(Borg._shared_state)
+print b.x, id(b.x), b.__dict__, id(b.__dict__), id(Borg._shared_state)
+# 9 140325309983816 {'x': 9} 140325311114016 140325311114016
+# 9 140325309983816 {'x': 9} 140325311114016 140325311114016
+print Borg._shared_state, id(Borg._shared_state)
+# {'x': 9} 140325311114016
 print '===================================================================================================='
 print '===================================================================================================='
 print '===================================================================================================='
---
+++ medusacode/python_demo/test__dict__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+"""
+In Python, everything is an object containing attributes.
+That includes classes themselves.
+The whole object-oriented thing with classes which define methods and instances
+which can execute those methods with the varying data per-instance is
+just a protocol for looking up attributes in a series of objects.
+"""
+class MyClass(object):
+    attr = 1
+
+    def __init__(self):
+        self.a = None
+        self.b = None
+
+    def geta(self):
+        return self.a
+
+    def getb(self):
+        return self.a
+
+    pass
+
+mc = MyClass()
+print MyClass.__dict__
+# {
+#   '__module__': '__main__',
+#   'attr': 1,
+#   'geta': <functiongetaat0x107bc5f50>,
+#   'getb': <functiongetbat0x107bd8050>,
+#   '__dict__': <attribute'__dict__'of'MyClass'objects>,
+#   '__weakref__': <attribute'__weakref__'of'MyClass'objects>,
+#   '__doc__': None,
+#   '__init__': <function__init__at0x107bc5ed8>
+# }
+print mc.__dict__
+# {
+#   'a': None,
+#   'b': None
+# }
+print dir(MyClass)
+print dir(mc)

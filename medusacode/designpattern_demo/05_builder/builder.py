dfcf979205157079f26c1a6bae4ab4a2bb54609f
gaohaoning <gaohaoning@breadtrip.com>
2016-03-18 21:28:00 +0800

    add builder pattern

--- medusacode/designpattern_demo/05_builder/builder.py
+++ medusacode/designpattern_demo/05_builder/builder.py
@@ -7,10 +7,10 @@
     建造者模式是一步一步创建一个复杂的对象，它允许用户只通过指定复杂对象的类型和内容就可以构建它们，用户不需要知道内部的具体构建细节。
 
 包含四个角色
+    [产品]是被构建的复杂对象，包含多个组成部件；
     [抽象建造者]为创建一个产品对象的各个部件指定抽象接口；
     [具体建造者]实现了抽象建造者接口，实现各个部件的构造和装配方法，定义并明确它所创建的复杂对象，也可以提供一个方法返回创建好的复杂产品对象；
-    [产品]是被构建的复杂对象，包含多个组成部件；
-    [指挥者]负责安排复杂对象的建造次序，指挥者与抽象建造者之间存在关联关系，可以在其construct()建造方法中调用建造者对象的部件构造与装配方法，完成复杂对象的建造
+    [指挥者]负责安排复杂对象的建造次序，指挥者与抽象建造者之间存在关联关系，可以在其建造方法中调用建造者对象的部件构造与装配方法，完成复杂对象的建造；
 
 在建造者模式的结构中引入了一个指挥者类，该类的作用主要有两个：
     一方面它隔离了客户与生产过程；
@@ -35,6 +35,71 @@
 建造者模式属于对象创建型模式。根据中文翻译的不同，建造者模式又可以称为生成器模式。
 """
 
+class Product(object):
+    """
+    产品
+    """
+    def __init__(self):
+        self.part_a = None
+        self.part_b = None
+    def __str__(self):
+        return '[product:%s]=[part_a:%s]+[part_b:%s]' % (self.__class__.__name__, self.part_a, self.part_b)
+
+class Builder_abstract(object):
+    """
+    抽象建造者
+    """
+    def __init__(self):
+        self.product = None
+    def new_product(self):
+        self.product = Product()
+    def build_partA(self):
+        pass
+    def build_partB(self):
+        pass
+
+class Builder_concrete_1(Builder_abstract):
+    """
+    具体建造者 1
+    """
+    def build_partA(self):
+        self.product.part_a = 'big Part_A'
+    def build_partB(self):
+        self.product.part_b = 'big Part_B'
+
+class Builder_concrete_2(Builder_abstract):
+    """
+    具体建造者 2
+    """
+    def build_partA(self):
+        self.product.part_a = 'small Part_A'
+    def build_partB(self):
+        self.product.part_b = 'small Part_B'
+
+class Director(object):
+    """
+    指挥者
+    """
+    def __init__(self, builder):
+        self.builder = builder
+    def build_parts(self):
+        self.builder.new_product()
+        self.builder.build_partA()
+        self.builder.build_partB()
+    def get_product(self):
+        return self.builder.product
 
 
+builder_1 = Builder_concrete_1()
+director_1 = Director(builder_1)
+director_1.build_parts()
+product_1 = director_1.get_product()
+print product_1
+# [product:Product]=[part_a:big Part_A]+[part_b:big Part_B]
 
+builder_2 = Builder_concrete_2()
+director_2 = Director(builder_2)
+director_2.build_parts()
+product_2 = director_2.get_product()
+print product_2
+# [product:Product]=[part_a:small Part_A]+[part_b:small Part_B]

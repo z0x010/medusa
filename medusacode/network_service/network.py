894738c34a19a9485386445af5ce6cce7e9c672a
gaohaoning <gaohaoning@breadtrip.com>
2016-03-25 19:51:24 +0800

    add comment for socket code

---
+++ medusacode/network_service/network.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""
+已经有如此多成熟优秀的框架（django，flask，pyramid），为什么要写 socket 程序 ？
+
+不闻不若闻之，闻之不若见之，见之不若知之，知之不若行之。
+I hear and I forget, I see and I remember, I do and I understand。
+"""

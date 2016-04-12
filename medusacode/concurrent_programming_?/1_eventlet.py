e28f9af247bc8e0bff53691b74655a59acbd4180
gaohaoning <gaohaoning@breadtrip.com>
2016-04-12 15:59:35 +0800

    add concurrent theory

---
+++ medusacode/concurrent_programming_?/1_eventlet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""
+eventlet 是基于 greenlet 实现的面向网络应用的并发处理框架，
+提供线程池、队列等与其他 Python 线程、进程模型非常相似的 api，
+并且提供了对 Python 发行版自带库及其他模块的超轻量并发适应性调整方法，
+比直接使用 greenlet 要方便得多。
+"""
